a12da193bb15de4d1c856c86f5cae6f8f393ad44
Niklas Johansson <raphexion@gmail.com>
2019-03-28 20:31:48 +0100

    Cleans-up kiks_consumer_sup

--- src/amqp/kiks_consumer_sup.erl
+++ src/amqp/kiks_consumer_sup.erl
@@ -2,21 +2,20 @@
 -behaviour(supervisor).
 -define(SERVER, ?MODULE).
 
--export([start_link/0]).
--export([init/1,
+-export([start_link/0,
 	 add_child/4]).
+-export([init/1]).
 
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 add_child(Exchange, Queue, RoutingKey, Listener) ->
-    add_child(#{exchange => support:b(Exchange),
-		queue => support:b(Queue),
-		routing_key => support:b(RoutingKey),
-		listener => Listener}).
+    Info = #{exchange => support:b(Exchange),
+	     queue => support:b(Queue),
+	     routing_key => support:b(RoutingKey),
+	     listener => Listener},
 
-add_child(Opts) ->
-    supervisor:start_child(?MODULE, [Opts]).
+    supervisor:start_child(?MODULE, [Info]).
 
 init(_) ->
     SupFlags = #{strategy => simple_one_for_one,
