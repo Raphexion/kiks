5e475e400b2a36547b47f41a647a6e90ea8aaf75
Niklas Johansson <raphexion@gmail.com>
2019-03-28 15:09:04 +0100

    Adds rebustness to amqp_common

--- src/amqp/amqp_common.erl
+++ src/amqp/amqp_common.erl
@@ -6,9 +6,9 @@
 	 ensure_queue/2]).
 
 ensure_exchange(Channel, Exchange) ->
-    Declare = #'exchange.declare'{exchange = Exchange, durable = true},
+    Declare = #'exchange.declare'{exchange = support:b(Exchange), durable = true},
     #'exchange.declare_ok'{} = amqp_channel:call(Channel, Declare).
 
 ensure_queue(Channel, Queue) ->
-    Declare = #'queue.declare'{queue = Queue, durable = true},
+    Declare = #'queue.declare'{queue = support:b(Queue), durable = true},
     #'queue.declare_ok'{} = amqp_channel:call(Channel, Declare).
