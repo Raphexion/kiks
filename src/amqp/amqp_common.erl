f0a349eb14586fb106b312fe7f4d87973caff25a
Niklas Johansson <raphexion@gmail.com>
2019-03-28 15:09:04 +0100

    Changes so exchanges and queues are durable

--- src/amqp/amqp_common.erl
+++ src/amqp/amqp_common.erl
@@ -6,9 +6,9 @@
 	 ensure_queue/2]).
 
 ensure_exchange(Channel, Exchange) ->
-    Declare = #'exchange.declare'{exchange = Exchange},
+    Declare = #'exchange.declare'{exchange = Exchange, durable = true},
     #'exchange.declare_ok'{} = amqp_channel:call(Channel, Declare).
 
 ensure_queue(Channel, Queue) ->
-    Declare = #'queue.declare'{queue = Queue},
+    Declare = #'queue.declare'{queue = Queue, durable = true},
     #'queue.declare_ok'{} = amqp_channel:call(Channel, Declare).
