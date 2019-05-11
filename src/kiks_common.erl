2cb925c124baede2c7ace9e024e665e3e444e916
Niklas Johansson <raphexion@gmail.com>
2019-05-11 12:38:07 +0200

    Adds functions to clean-up
    Features to:
    - delete exchange
    - delete queue
    - unbind queue
    
    In addtion, make the code smaller by importing support:b.

--- src/kiks_common.erl
+++ src/kiks_common.erl
@@ -2,15 +2,34 @@
 
 -include_lib("amqp_client/include/amqp_client.hrl").
 
+-import(support, [b/1]).
+
 -export([ensure_exchange/2,
 	 ensure_queue/2]).
+-export([delete_exchange/2,
+	 delete_queue/2,
+	 unbind_queue/4]).
 
 ensure_exchange(Channel, Exchange) ->
-    Declare = #'exchange.declare'{exchange = support:b(Exchange),
+    Declare = #'exchange.declare'{exchange = b(Exchange),
 				  type = <<"topic">>,
 				  durable = true},
     #'exchange.declare_ok'{} = amqp_channel:call(Channel, Declare).
 
 ensure_queue(Channel, Queue) ->
-    Declare = #'queue.declare'{queue = support:b(Queue), durable = true},
+    Declare = #'queue.declare'{queue = b(Queue), durable = true},
     #'queue.declare_ok'{} = amqp_channel:call(Channel, Declare).
+
+delete_exchange(Channel, Exchange) ->
+    Delete = #'exchange.delete'{exchange = b(Exchange)},
+    #'exchange.delete_ok'{} = amqp_channel:call(Channel, Delete).
+
+delete_queue(Channel, Queue) ->
+    Delete = #'queue.delete'{queue = b(Queue)},
+    #'queue.delete_ok'{} = amqp_channel:call(Channel, Delete).
+
+unbind_queue(Channel, Queue, Exchange, RoutingKey) ->
+    Binding = #'queue.bind'{queue       = b(Queue),
+			    exchange    = b(Exchange),
+			    routing_key = b(RoutingKey)},
+    #'queue.bind_ok'{} = amqp_channel:call(Channel, Binding).
