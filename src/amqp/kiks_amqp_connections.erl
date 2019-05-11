9a694f418993dfcf84473217426640f513f7c5d4
Niklas Johansson <raphexion@gmail.com>
2019-05-11 13:01:13 +0200

    Moves get_config_as_string to support

--- src/amqp/kiks_amqp_connections.erl
+++ src/amqp/kiks_amqp_connections.erl
@@ -3,6 +3,9 @@
 
 -include_lib("amqp_client/include/amqp_client.hrl").
 
+-import(support, [get_config_as_string/2,
+		  get_config_as_binary/2]).
+
 -define(SERVER, ?MODULE).
 
 %% API
@@ -28,24 +31,6 @@ start_link() ->
 
 get() ->
     gen_server:call(?SERVER, get).
-
-get_config_as_string(Name, Default) ->
-    Env = atom_to_list(Name),
-    get_config_as_string(Name, Default, os:getenv(Env)).
-
-get_config_as_string(Name, Default, false) ->
-    case application:get_env(kiks, Name) of
-	{ok, Value} ->
-	    Value;
-	_ ->
-	    Default
-    end;
-get_config_as_string(_Name, _Default, Value) ->
-    Value.
-
-get_config_as_binary(Name, Default) ->
-    String = get_config_as_string(Name, Default),
-    list_to_binary(String).
 
 %%-----------------------------------------------------------------------------
 %% Behaviour callbacks
--- src/support/support.erl
+++ src/support/support.erl
@@ -1,5 +1,25 @@
 -module(support).
 
--export([b/1]).
+-export([b/1,
+	 get_config_as_string/2,
+	 get_config_as_binary/2]).
 
 b(Variable) when is_binary(Variable) ->                                                                                                              Variable;                                                                                                                                    b(Variable) when is_list(Variable) ->                                                                                                                list_to_binary(Variable).
+
+get_config_as_string(Name, Default) ->
+    Env = atom_to_list(Name),
+    get_config_as_string(Name, Default, os:getenv(Env)).
+
+get_config_as_string(Name, Default, false) ->
+    case application:get_env(kiks, Name) of
+	{ok, Value} ->
+	    Value;
+	_ ->
+	    Default
+    end;
+get_config_as_string(_Name, _Default, Value) ->
+    Value.
+
+get_config_as_binary(Name, Default) ->
+    String = get_config_as_string(Name, Default),
+    list_to_binary(String).
